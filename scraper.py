8fb6f4d3b1eadd0d21e6650d38c8f41528d6ced4
Andrew Chen <moorheadandrew@gmail.com>
2015-04-05 23:00:54 -0700

    Inserts scraped data into Cloud SQL. Deletes previous check_date,weekend,destination data to prevent duplicates.

--- scraper.py
+++ scraper.py
@@ -6,6 +6,16 @@ from selenium.webdriver.common.keys import Keys
 
 url_template = 'https://www.google.com/flights/#search;f={origin};t={destination};d={depart_date};r={return_date};ti=t{depart_times},l{arrival_times}'
 
+def execute_sql(sql):
+  try:
+    cursor.execute(sql)
+    db.commit()
+    print "db insert success"
+  except MySQLdb.Error as e:
+    db.rollback()
+    print "db insert error"
+    print e
+
 def generate_weekend_dates(num_weeks):
   weekend_dates = []
   nearest_friday = datetime.date.today() + datetime.timedelta( (4-datetime.date.today().weekday()) %7)
@@ -24,7 +34,7 @@ weekend_dates = generate_weekend_dates(24)
 #destinations = ["AUS"]
 #weekend_dates = generate_weekend_dates(1)
 
-db = MySQLdb.connect("173.194.80.20","root","roos","guestbook")
+db = MySQLdb.connect("173.194.80.20","root","roos","weekendfares")
 cursor=db.cursor()
 
 driver = webdriver.Chrome('/Applications/chromedriver')
@@ -59,8 +69,21 @@ for d in destinations:
           prices.append(price)
       best_flight = flights_by_price[min(prices)]
 
+    del best_flight[1]
+    best_flight = [datetime.date.today().isoformat(), weekend[0], weekend[1], d] + best_flight
     print best_flight
+
+    delete_sql="""DELETE FROM fares WHERE check_date = '%s' and there_date = '%s' and destination_airport = '%s'""" % (best_flight[0], best_flight[1], best_flight[3])
+
+    execute_sql(delete_sql)
+
+    insert_sql="""INSERT INTO fares (check_date, there_date, back_date, destination_airport, price, there_times, there_operator, there_time, there_stops) VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s')""" % (best_flight[0], best_flight[1], best_flight[2], best_flight[3], best_flight[4], best_flight[5], best_flight[6], best_flight[7], best_flight[8])
+
+    print insert_sql
+
+    execute_sql(insert_sql)
+
+db.close()
 driver.close()
 
-#sql="""CREATE TABLE test(andrew INT, jenny INT)"""
-#cursor.execute(sql)
+

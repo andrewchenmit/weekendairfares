a21338cbacf392e0810867fc69a4977ad32246ba
Andrew Chen <moorheadandrew@gmail.com>
2015-04-14 00:24:30 -0700

    Cleaned up config code to be flexible. Created 3 day weekend config.

---
+++ config.py
@@ -0,0 +1,35 @@
+import config_utility
+
+two_day_weekend = config_utility.ScraperConfig([
+  'PDX',
+  'SAN',
+  'YYJ',
+  'YVR',
+  'PHX',
+  'SEA',
+  'SLC',
+  'LAS',
+  'LAX',
+  'DEN',
+  'PSP'],
+  config_utility.generate_weekend_dates(24),
+  '2000-2400',
+  '1600-2200',
+  'fares'
+)
+
+three_day_dests = config_utility.ScraperConfig([
+  'SJD',
+  'YYC',
+  'PVR',
+  'AUS',
+  'SAT',
+  'ORD,MDW',
+  'STL',
+  'JAC'],
+  config_utility.generate_three_day_weekend_dates(24),
+  '2000-2400',
+  '1600-2200',
+  '3dayweekends'
+)
+
---
+++ config_utility.py
@@ -0,0 +1,42 @@
+import datetime
+
+class ScraperConfig:
+  def __init__(self, dests, date_pairs, depart_times, arrival_times, sql_table):
+    self.dests = dests
+    self.date_pairs = date_pairs
+    self.depart_times = depart_times
+    self.arrival_times = arrival_times
+    self.sql_table = sql_table
+
+def generate_weekend_dates(num_weeks):
+  weekend_dates = []
+  starting_date = datetime.date(2015,4,4)
+  nearest_friday = starting_date + datetime.timedelta( (4-starting_date.weekday()) %7)
+  for i in xrange(num_weeks):
+    friday = nearest_friday + datetime.timedelta(i*7)
+    sunday = friday + datetime.timedelta(2)
+    weekend_dates.append([
+      str(friday.year)+'-'+str('{:02d}'.format(friday.month))+'-'+str('{:02d}'.format(friday.day)),
+      str(sunday.year)+'-'+str('{:02d}'.format(sunday.month))+'-'+str('{:02d}'.format(sunday.day))
+      ])
+  return weekend_dates
+
+def generate_three_day_weekend_dates(num_weeks):
+  weekend_dates = []
+  starting_date = datetime.date(2015,4,4)
+  nearest_thursday = starting_date + datetime.timedelta( (3-starting_date.weekday()) %7)
+  for i in xrange(num_weeks):
+    thursday = nearest_thursday + datetime.timedelta(i*7)
+    friday = thursday + datetime.timedelta(1)
+    sunday = friday + datetime.timedelta(2)
+    monday = sunday + datetime.timedelta(1)
+
+    weekend_dates.append([
+      str(thursday.year)+'-'+str('{:02d}'.format(thursday.month))+'-'+str('{:02d}'.format(thursday.day)),
+      str(sunday.year)+'-'+str('{:02d}'.format(sunday.month))+'-'+str('{:02d}'.format(sunday.day))
+      ])
+    weekend_dates.append([
+      str(friday.year)+'-'+str('{:02d}'.format(friday.month))+'-'+str('{:02d}'.format(friday.day)),
+      str(monday.year)+'-'+str('{:02d}'.format(monday.month))+'-'+str('{:02d}'.format(monday.day))
+      ])
+  return weekend_dates

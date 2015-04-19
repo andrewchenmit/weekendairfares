fb0458c9f3a223ab0b0b71f8841e12839138f4b9
Andrew Chen <moorheadandrew@gmail.com>
2015-04-18 23:53:38 -0700

    Don't include 2 day locations in 3 day. Reduce weekends to check down to 4 months out.

--- config/config.py
+++ config/config.py
@@ -2,9 +2,11 @@ import times
 import destinations
 import utility
 
+weeks = 16
+
 friday_evening_to_sunday_evening = utility.ScraperConfig(
   destinations.short_dist_dests,
-  utility.gen_date_pairs('friday', 'sunday', 24),
+  utility.gen_date_pairs('friday', 'sunday', weeks),
   times.depart_evening,
   times.arrive_evening,
   'Friday evening',
@@ -14,7 +16,7 @@ friday_evening_to_sunday_evening = utility.ScraperConfig(
 
 friday_evening_to_monday_morning = utility.ScraperConfig(
   destinations.short_dist_dests,
-  utility.gen_date_pairs('friday', 'monday', 24),
+  utility.gen_date_pairs('friday', 'monday', weeks),
   times.depart_evening,
   times.arrive_morning,
   'Friday evening',
@@ -23,8 +25,8 @@ friday_evening_to_monday_morning = utility.ScraperConfig(
 )
 
 thursday_evening_to_sunday_evening = utility.ScraperConfig(
-  destinations.short_dist_dests + destinations.medium_dist_dests,
-  utility.gen_date_pairs('thursday', 'sunday', 24),
+  destinations.medium_dist_dests,
+  utility.gen_date_pairs('thursday', 'sunday', weeks),
   times.depart_evening,
   times.arrive_evening,
   'Thursday evening',
@@ -33,8 +35,8 @@ thursday_evening_to_sunday_evening = utility.ScraperConfig(
 )
 
 thursday_evening_to_monday_morning = utility.ScraperConfig(
-  destinations.short_dist_dests + destinations.medium_dist_dests,
-  utility.gen_date_pairs('thursday', 'monday', 24),
+  destinations.medium_dist_dests,
+  utility.gen_date_pairs('thursday', 'monday', weeks),
   times.depart_evening,
   times.arrive_morning,
   'Thursday evening',
@@ -43,8 +45,8 @@ thursday_evening_to_monday_morning = utility.ScraperConfig(
 )
 
 friday_evening_to_monday_evening = utility.ScraperConfig(
-  destinations.short_dist_dests + destinations.medium_dist_dests,
-  utility.gen_date_pairs('friday', 'monday', 24),
+  destinations.medium_dist_dests,
+  utility.gen_date_pairs('friday', 'monday', weeks),
   times.depart_evening,
   times.arrive_evening,
   'Friday evening',
@@ -53,8 +55,8 @@ friday_evening_to_monday_evening = utility.ScraperConfig(
 )
 
 friday_evening_to_tuesday_morning = utility.ScraperConfig(
-  destinations.short_dist_dests + destinations.medium_dist_dests,
-  utility.gen_date_pairs('friday', 'tuesday', 24),
+  destinations.medium_dist_dests,
+  utility.gen_date_pairs('friday', 'tuesday', weeks),
   times.depart_evening,
   times.arrive_morning,
   'Friday evening',

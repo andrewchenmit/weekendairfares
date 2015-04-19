4ddf1d5190a6ce235d7a8a9cbc57bd496458b742
Andrew Chen <moorheadandrew@gmail.com>
2015-04-19 14:05:52 -0700

    Update scraper to clear memcache after each scrape.

--- scraper.py
+++ scraper.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
+import urllib2
 from config import airfare_scraper
 from config import config
 
 for c in config.configs_to_scrape:
   scraper = airfare_scraper.AirfareScraper(c, overwrite=False)
   scraper.scrape()
+  result = urllib2.urlopen("http://www.andrewchen.us/clearmemcache").read()
+  print "Clear memcache result: ", result
